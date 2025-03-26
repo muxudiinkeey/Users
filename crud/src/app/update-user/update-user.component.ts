2d1c6b152d385ced60fd7483209026a80ae345ba
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-26 10:01:21 +0100

    ok

--- crud/src/app/update-user/update-user.component.ts
+++ crud/src/app/update-user/update-user.component.ts
@@ -9,21 +9,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './update-user.component.css'
 })
 export class UpdateUserComponent implements OnInit{
-crud = inject (CrudService);
-activateRoute = inject (ActivatedRoute)
-  userId!: {
-    uid: number;
-  };
+
+
+  
 ngOnInit(): void {
-  this.userId = {
-    uid : this.activateRoute.snapshot.params['id']
-  }
-  console.log(this.userId.uid)
+
 }
 
-onGetById(){
 
 
-  //this.crud.getDataById()
+
 }
-}
+
